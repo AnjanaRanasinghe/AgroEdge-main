25b451db4b0086f752da6286c1d10cc8d925793b
Anjana Ranasinghe <anjana.20231160@iit.ac.lk>
2025-11-11 21:56:58 +0530

    Updated the folder paths and remove things

--- server/management/routes/uploadRoutes.js
+++ server/management/routes/uploadRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
-const uploadController = require('../../controllers/uploadController');
-const authMiddleware = require('../../middleware/auth');
+const uploadController = require('../controllers/uploadController');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
